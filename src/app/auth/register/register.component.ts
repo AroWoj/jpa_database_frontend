270bcf54672625fb4b9f6f45ec32e78942ef3ef0
voytasic <arekw37@o2.pl>
2025-08-25 23:34:14 +0200

    Change to angular material

--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -1,17 +1,31 @@
-import { Component } from '@angular/core';
-import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { Component, signal } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { Router, RouterLink } from '@angular/router';
 import { AuthService } from '../auth.service';
+import { CommonModule } from '@angular/common';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-register',
-  imports: [ReactiveFormsModule, RouterLink],
+  standalone: true,
+  imports: [
+    ReactiveFormsModule,
+    RouterLink,
+    CommonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatIconModule
+  ],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
 export class RegisterComponent {
   errorMsg = '';
-
+  hide = signal(true);
   form: FormGroup;
   constructor(
     private authService: AuthService,
@@ -19,28 +33,32 @@ export class RegisterComponent {
     private router: Router
   ) {
     this.form = this.fb.group({
-      name: [''],
-      email: [''],
-      password: ['']
+      name: ['', [Validators.required, Validators.minLength(2)]],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(8)]]
     })
   }
-
+  clickEvent(event: MouseEvent) {
+    this.hide.set(!this.hide());
+    event.stopPropagation();
+  }
   submit(userData: {
     name: string,
     email: string,
     password: string
   }) {
-   
 
-    this.authService.register(userData).subscribe({next: (data)=> {
-       console.log("register userData: ", data)
-      this.router.navigate(['/activate-account']); 
-    },
-     error: (err) => this.errorMsg = err.error.message || 'Registration failed, please try again.'
+
+    this.authService.register(userData).subscribe({
+      next: (data) => {
+        console.log("register userData: ", data)
+        this.router.navigate(['/activate-account']);
+      },
+      error: (err) => this.errorMsg = err.error.message || 'Registration failed, please try again.'
     })
-   
+
   }
   cancel() {
-  this.form.reset();
-}
+    this.form.reset();
+  }
 }
