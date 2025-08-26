fa56dc50659b6b7c3396ad93615543066bc9af02
voytasic <arekw37@o2.pl>
2025-08-26 02:06:52 +0200

    Update app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,15 +3,35 @@ import { RouterOutlet } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from "./components/header/header.component";
 import { FooterComponent } from "./components/footer/footer.component";
+import { LoadingService } from './services/loading.service';
+import { delay } from 'rxjs';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
 @Component({
     selector: 'app-root',
-    imports: [RouterOutlet, CommonModule, HeaderComponent, FooterComponent],
+    imports: [RouterOutlet, CommonModule, HeaderComponent, FooterComponent, MatProgressSpinnerModule],
     templateUrl: './app.component.html',
     styleUrl: './app.component.css'
 })
-export class AppComponent {
- 
+export class AppComponent implements OnInit {
+    loading: boolean = false;
+    title = 'jpa_database_frontend';
+    constructor(private _loading: LoadingService) { }
+
+    ngOnInit(): void {
+        this.listenToLoading();
+    }
+
+    /**
+   * Listen to the loadingSub property in the LoadingService class. This drives the
+   * display of the loading spinner.
+   */
+    listenToLoading(): void {
+        this._loading.loadingSub
+            .pipe(delay(0)) // This prevents a ExpressionChangedAfterItHasBeenCheckedError for subsequent requests
+            .subscribe((loading) => {
+                this.loading = loading;
+            });
+    }
 }
-   
-   
+
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,12 +4,13 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { HTTP_INTERCEPTORS, provideHttpClient, withInterceptors } from '@angular/common/http';
 import { authInterceptor } from './auth/interceptor/auth.interceptor';
+import { spinnerInterceptor } from './interceptors/spinner.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideZoneChangeDetection({ eventCoalescing: true }), 
+    provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    provideHttpClient(withInterceptors([authInterceptor])),
+    provideHttpClient(withInterceptors([authInterceptor, spinnerInterceptor])),
     //{ provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true }
   ]
 };
